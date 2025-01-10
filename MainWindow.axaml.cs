470144175344570fae283264af24d6603f8c7836
isaiahmartin <isaiah@cei-engineering.com>
2025-01-10 15:21:36 -0500

    Initial commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace CalcpadGenerator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
